d1b31ccf79f522624677e69f0305d356cb7adcc0
mustafa arkan <104664022+mustafaarkan1@users.noreply.github.com>
2025-01-30 23:21:32 +0300

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,15 @@
+
+/*
+ *  Copyright (c) 2024 Mustafa Arkan. All rights reserved.
+ *  
+ *  This source code is licensed under the MIT license
+ *  found in the LICENSE file in the root directory of this project.
+ *  https://www.instagram.com/4m.u1
+ *  www.linkedin.com/in/mustafa-arkan
+ *
+ */
+
+
 import 'package:flutter/material.dart';
 import 'about_tab.dart';
 import 'create_tab.dart';
